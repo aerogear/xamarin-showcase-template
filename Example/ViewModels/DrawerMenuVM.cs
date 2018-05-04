2bab297ebc9f14e9451652e02f56b3593d4c613f
Vojtěch Sázel <vojtech@sazel.cz>
2018-05-04 10:18:35 +0200

    Example app imported

---
+++ Example/ViewModels/DrawerMenuVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Example.Models;
+using Example.Resources;
+using Example.Utils;
+using Example.ViewModels.Base;
+using Example.Views.Pages;
+
+namespace Example.ViewModels
+{
+    public class DrawerMenuVM : BaseVM
+    {
+        public ObservableCollection<NavigationItem> NavigationItems { get; set; }
+
+        public DrawerMenuVM()
+        {
+            NavigationItems = new ObservableCollection<NavigationItem>(new[]
+            {
+                    NavigationItem.HOME_PAGE,
+                    new NavigationItem { Id = 1, Title = StringResources.NavHttp, TargetType=typeof(HttpPage),Icon=ResourceUtils.GetSvg("ic_http") },
+                    new NavigationItem { Id = 2, Title = StringResources.NavAuth, TargetType=typeof(AuthPage),Icon=ResourceUtils.GetSvg("ic_auth") },
+                });
+        }
+    }
+}
