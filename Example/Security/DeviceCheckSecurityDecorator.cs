884f75b7eaca1a329bb6a76b68e3ce5573484715
Massimiliano Ziccardi <mziccard@redhat.com>
2018-06-08 12:21:30 +0200

    Added custom message for each iOS check Removed decimals from trustscore

---
+++ Example/Security/DeviceCheckSecurityDecorator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using AeroGear.Mobile.Security;
+using Xamarin.Forms;
+
+namespace Example.Security
+{
+    internal class DeviceCheckSecurityDecorator : ISecurityCheck
+    {
+        private readonly ISecurityCheck securityCheck;
+        private readonly bool secureWhenFailed;
+        private readonly string secureMessage;
+        private readonly string unsecureMessage;
+
+        public DeviceCheckSecurityDecorator(ISecurityCheck securityCheck, string secureMessage, string unsecureMessage, bool secureWhenFailed = false)
+        {
+            this.securityCheck = securityCheck;
+            this.secureWhenFailed = secureWhenFailed;
+            this.secureMessage = secureMessage;
+            this.unsecureMessage = unsecureMessage;
+        }
+
+        public string GetId() => securityCheck.GetId();
+
+        public string GetName() => securityCheck.GetName();
+
+        public SecurityCheckResult Check()
+        {
+            return new SecurityCheckResultDecorator(securityCheck.Check(), secureMessage, unsecureMessage, secureWhenFailed);
+        }
+    }
+
+    public class SecurityCheckResultDecorator : SecurityCheckResult
+    {
+        public readonly bool IsSecure;
+        private readonly string secureMessage;
+        private readonly string unsecureMessage;
+        private readonly SecurityCheckResult result;
+
+        public SecurityCheckResultDecorator(SecurityCheckResult result, string secureMessage, string unsecureMessage, bool secureWhenFailed)
+        {
+            IsSecure = secureWhenFailed ? !result.Passed : result.Passed;
+            this.secureMessage = secureMessage;
+            this.unsecureMessage = unsecureMessage;
+            this.result = result;
+        }
+        
+        public string Message => IsSecure? secureMessage : unsecureMessage;
+
+        public Color DisplayColor => IsSecure ? Color.Green : Color.Red;
+    }   
+}
--- Example/Views/Pages/SecurityCheckPage.xaml.cs
+++ Example/Views/Pages/SecurityCheckPage.xaml.cs
@@ -4,9 +4,9 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
 using AeroGear.Mobile.Core;
+using AeroGear.Mobile.Core.Utils;
 using AeroGear.Mobile.Security;
 using Example.Security;
-using Example.ViewModels.Security;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,13 +15,13 @@ namespace Example.Views.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SecurityCheckPage : ContentPage, INotifyPropertyChanged
     {
-        private readonly static int TRUST_SCORE_DECIMAL_PLACE_COUNT = 2;
+        private readonly static int TRUST_SCORE_DECIMAL_PLACE_COUNT = 0;
         private readonly static decimal TRUST_SCORE_INITIAL_PERCENTAGE = 0;
 
-        private ObservableCollection<SecurityCheckResultReport> securityCheckResults = new ObservableCollection<SecurityCheckResultReport>();
+        private ObservableCollection<SecurityCheckResultDecorator> securityCheckResults = new ObservableCollection<SecurityCheckResultDecorator>();
         private decimal trustScore = TRUST_SCORE_INITIAL_PERCENTAGE;
 
-        public ObservableCollection<SecurityCheckResultReport> CheckResults
+        public ObservableCollection<SecurityCheckResultDecorator> CheckResults
         {
             get { return this.securityCheckResults; }
         }
@@ -35,21 +35,58 @@ namespace Example.Views.Pages
             InitializeComponent();
             BindingContext = this;
 
-            var securityChecks = DependencyService.Get<ISecurityCheckProvider>().SecurityChecks;
+            //var securityChecks = DependencyService.Get<ISecurityCheckProvider>().SecurityChecks;
+            ISecurityCheckFactory securityCheckFactory = ServiceFinder.Resolve<ISecurityCheckFactory>();
+
+            var securityChecks = new List<ISecurityCheck>();
+
+            // Configure securitychecks list
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                securityChecks.Add(
+                    new DeviceCheckSecurityDecorator(
+                        securityCheckFactory.create("DeviceLockCheck"),
+                        "Device Lock Enabled",
+                        "No Device Lock Enabled"
+                    )); 
+                securityChecks.Add(
+                    new DeviceCheckSecurityDecorator(
+                        securityCheckFactory.create("NoDebuggerCheck"),
+                        "No Debugger Detected",
+                        "Debugger Detected"
+                    )); 
+                securityChecks.Add(
+                    new DeviceCheckSecurityDecorator(
+                        securityCheckFactory.create("NonJailbrokenCheck"),
+                        "No Jailbreak Detected",
+                        "Jailbreak Detected"
+                    )); 
+                securityChecks.Add(
+                    new DeviceCheckSecurityDecorator(
+                        securityCheckFactory.create("NotInEmulatorCheck"),
+                        "No Emulator Access Detected",
+                        "Emulator Access Detected"
+                    )); 
+            }
+            else if (Device.RuntimePlatform == Device.Android)
+            {
+
+            }
+
             ReportCheckResults(securityChecks);
         }
 
-        private void ReportCheckResults(ICollection<ISecurityCheckType> securityChecks)
+        private void ReportCheckResults(ICollection<ISecurityCheck> securityChecks)
         {
             var securityService = MobileCore.Instance.GetService<ISecurityService>();
             foreach (var check in securityChecks)
             {
                 var checkResult = securityService.Check(check);
                 // Convert to a security check result report to include UI specific information.
-                securityCheckResults.Add(new SecurityCheckResultReport(checkResult));
+                securityCheckResults.Add((SecurityCheckResultDecorator)checkResult);
             }
 
-            this.trustScore = CalculateTrustScore(new List<SecurityCheckResultReport>(securityCheckResults));
+            this.trustScore = CalculateTrustScore(new List<SecurityCheckResultDecorator>(securityCheckResults));
             NotifyPropertyChanged(nameof(TrustScore));
         }
 
@@ -59,9 +96,9 @@ namespace Example.Views.Pages
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        private decimal CalculateTrustScore(List<SecurityCheckResultReport> results)
+        private decimal CalculateTrustScore(List<SecurityCheckResultDecorator> results)
         {
-            int failedResultCount = results.FindAll(result => !result.Passed).Count;
+            int failedResultCount = results.FindAll(result => !result.IsSecure).Count;
             return 100 - Math.Round(Decimal.Divide(failedResultCount, results.Count) * 100, TRUST_SCORE_DECIMAL_PLACE_COUNT);
         }
     }
