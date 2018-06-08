3fce1edaf734c8bdd378ce352438fe2c194a9b38
Massimiliano Ziccardi <mziccard@redhat.com>
2018-06-08 12:35:26 +0200

    Added custom message for each android check

--- Example/Views/Pages/SecurityCheckPage.xaml.cs
+++ Example/Views/Pages/SecurityCheckPage.xaml.cs
@@ -36,44 +36,107 @@ namespace Example.Views.Pages
             BindingContext = this;
 
             //var securityChecks = DependencyService.Get<ISecurityCheckProvider>().SecurityChecks;
-            ISecurityCheckFactory securityCheckFactory = ServiceFinder.Resolve<ISecurityCheckFactory>();
 
-            var securityChecks = new List<ISecurityCheck>();
+
+            List<ISecurityCheck> securityChecks = new List<ISecurityCheck>();
 
             // Configure securitychecks list
             if (Device.RuntimePlatform == Device.iOS)
             {
-                securityChecks.Add(
-                    new DeviceCheckSecurityDecorator(
-                        securityCheckFactory.create("DeviceLockCheck"),
-                        "Device Lock Enabled",
-                        "No Device Lock Enabled"
-                    )); 
-                securityChecks.Add(
-                    new DeviceCheckSecurityDecorator(
-                        securityCheckFactory.create("NoDebuggerCheck"),
-                        "No Debugger Detected",
-                        "Debugger Detected"
-                    )); 
-                securityChecks.Add(
-                    new DeviceCheckSecurityDecorator(
-                        securityCheckFactory.create("NonJailbrokenCheck"),
-                        "No Jailbreak Detected",
-                        "Jailbreak Detected"
-                    )); 
-                securityChecks.Add(
-                    new DeviceCheckSecurityDecorator(
-                        securityCheckFactory.create("NotInEmulatorCheck"),
-                        "No Emulator Access Detected",
-                        "Emulator Access Detected"
-                    )); 
+                securityChecks = getIOSChecks();
             }
             else if (Device.RuntimePlatform == Device.Android)
             {
-
+                securityChecks = getAndroidChecks();
             }
 
             ReportCheckResults(securityChecks);
+        }
+
+        private List<ISecurityCheck> getIOSChecks()
+        {
+            ISecurityCheckFactory securityCheckFactory = ServiceFinder.Resolve<ISecurityCheckFactory>();
+            var securityChecks = new List<ISecurityCheck>();
+
+            // Configure securitychecks list
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("DeviceLockCheck"),
+                    "Device Lock Enabled",
+                    "No Device Lock Enabled"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("NoDebuggerCheck"),
+                    "No Debugger Detected",
+                    "Debugger Detected"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("NonJailbrokenCheck"),
+                    "No Jailbreak Detected",
+                    "Jailbreak Detected"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("NotInEmulatorCheck"),
+                    "No Emulator Access Detected",
+                    "Emulator Access Detected"
+                ));
+
+            return securityChecks;
+        }
+
+        private List<ISecurityCheck> getAndroidChecks()
+        {
+            ISecurityCheckFactory securityCheckFactory = ServiceFinder.Resolve<ISecurityCheckFactory>();
+            var securityChecks = new List<ISecurityCheck>();
+
+            // Configure securitychecks list
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("BackupDisallowedCheck"),
+                    "App Data Backup Recovery Disabled",
+                    "App Data Backup Recovery Enabled"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("DeveloperModeDisabledCheck"),
+                    "Developer Options Disabled",
+                    "Developer Options Enabled"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("EncryptionCheck"),
+                    "Device Encryption Active",
+                    "Device Encryption Inactive"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("NoDebuggerCheck"),
+                    "No Debugger Detected",
+                    "Debugger Detected"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("NonRootedCheck"),
+                    "No Root Access Detected",
+                    "Root Access Detected"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("NotInEmulatorCheck"),
+                    "No Emulator Access Detected",
+                    "Emulator Access Detected"
+                ));
+            securityChecks.Add(
+                new DeviceCheckSecurityDecorator(
+                    securityCheckFactory.create("ScreenLockCheck"),
+                    "Device Lock Enabled",
+                    "No Device Lock Enabled"
+                ));
+
+            return securityChecks;
         }
 
         private void ReportCheckResults(ICollection<ISecurityCheck> securityChecks)
