84dc85eb1a3a72fd1d5bf89f891c2f9250f4f403
Massimiliano Ziccardi <mziccard@redhat.com>
2018-06-14 17:48:29 +0200

    Added user details

--- Example/Views/Pages/UserDetails.xaml.cs
+++ Example/Views/Pages/UserDetails.xaml.cs
@@ -1,39 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using AeroGear.Mobile.Auth;
 using AeroGear.Mobile.Core;
 using Xamarin.Forms;
 
 namespace Example.Views.Pages
 {
-    public partial class UserDetails : ContentPage
+    public partial class UserDetails : ContentPage, INotifyPropertyChanged
     {
+        private User user;
+        private ObservableCollection<UserRole> userRoles = new ObservableCollection<UserRole>();
+
         public UserDetails()
         {
+            this.user = MobileCore.Instance.GetService<IAuthService>().CurrentUser();
+            userRoles = new ObservableCollection<UserRole>(this.user.Roles);
+            //NotifyPropertyChanged(nameof(AssignedRoles));
             InitializeComponent();
-            InitListView();
+            BindingContext = this;
+            //NotifyPropertyChanged(nameof(AssignedRoles));
+            //InitListView();
         }
 
-        private void InitListView() {
-            var user = MobileCore.Instance.GetService<IAuthService>().CurrentUser();
-            var roleItems = new List<string> { };
-            foreach(var role in user.getRoles())
+        //private void InitListView()
+        //{
+        //    var user = MobileCore.Instance.GetService<IAuthService>().CurrentUser();
+        //    var roleItems = new List<string> { };
+        //    foreach (var role in user.getRoles())
+        //    {
+        //        roleItems.Add(role.ToString());
+        //    }
+
+        //    lstRoles.ItemsSource = roleItems;
+        //    //username.Text = user.Username;
+        //    //email.Text = user.Email;
+        //}
+
+        public ICollection<UserRole> AssignedRoles 
+        {
+            get
             {
-                roleItems.Add(role.ToString());
+                return userRoles;
             }
-
-            lstRoles.ItemsSource = roleItems;
-            username.Text = user.Username;
-            email.Text = user.Email;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
 
         void onLogoutClicked(object sender, System.EventArgs e)
         {
-            var authService = MobileCore.Instance.GetService<IAuthService>();
-            authService.Logout(authService.CurrentUser()).ContinueWith(result =>
-            {
-                Device.BeginInvokeOnMainThread(() => Navigation.PushAsync(new AuthPage()));
-            });
+            //var authService = MobileCore.Instance.GetService<IAuthService>();
+            //authService.Logout(authService.CurrentUser()).ContinueWith(result =>
+            //{
+            //    Device.BeginInvokeOnMainThread(() => Navigation.PushAsync(new AuthPage()));
+            //});
         }
     }
 }
