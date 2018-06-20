7e91986e68428cc8191e6c204f7cf57b201999f5
Massimiliano Ziccardi <mziccard@redhat.com>
2018-06-20 17:49:14 +0200

    Added WebView for docs

---
+++ Example/Models/WebNavigationItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Example.Views.Pages;
+using Xamarin.Forms;
+
+namespace Example.Models
+{
+    public class WebNavigationItem : NavigationItem
+    {
+        public string Source { get; set; }
+
+        public override Page Page => new WebPage(Source);
+
+        public WebNavigationItem()
+        {
+            this.TargetType = typeof(WebPage);
+        }
+    }
+}
